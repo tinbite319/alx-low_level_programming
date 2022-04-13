913f3f2f806d52442e868e9a0b6297439871be2a
tinbite319 <tinbite792@gmail.com>
2022-04-12 22:20:25 -0700

    commit 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabet - prints alphabet in lower case
+* main - check the code.
+*
+*Return: Always 0.
+*/
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+{
+
+		_putchar(c);
+}
+	_putchar('\n');
+}
